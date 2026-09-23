7e9e7f90bba1ce7fd0ca0d07b7ccf3ee1e06682e
Mehmet Faruk Gul <m.farukgul@gmail.com>
2026-09-23 17:03:10 +0300

    First Commit.

---
+++ sw.js
@@ -0,0 +1,16 @@
+{
+  "name": "E-Künye",
+  "short_name": "E-Künye",
+  "description": "Kişisel bilgilerini tek dokunuşla kopyala.",
+  "lang": "tr",
+  "start_url": "./",
+  "scope": "./",
+  "display": "standalone",
+  "background_color": "#FFFFFF",
+  "theme_color": "#FFFFFF",
+  "icons": [
+    { "src": "icons/icon-192.png", "sizes": "192x192", "type": "image/png" },
+    { "src": "icons/icon-512.png", "sizes": "512x512", "type": "image/png" },
+    { "src": "icons/icon-512.png", "sizes": "512x512", "type": "image/png", "purpose": "maskable" }
+  ]
+}
